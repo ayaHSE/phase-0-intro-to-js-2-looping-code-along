76b2e0b7d009501c9a8fdecf66775ae551a0ed2c
Aya Hassan <147654234+ayaHSE@users.noreply.github.com>
2024-02-22 17:10:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+        let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+        thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+module.exports = {
+    writeCards,
+    countDown
+};
